10638a66a5bc6db2f263604e39477843f4f4e04a
Daniel van Duinkerken <daanivd@hotmail.com>
2018-10-26 12:56:42 +0000

    added picture scrolling functionality

--- js/script.js
+++ js/script.js
@@ -1,50 +1,75 @@
 
 //waits until page is ready
 $(document).ready(function() {
-// FILTER BUTTONS //    
-$("#landscapes").click(function(){
-    $(".thumb").css("display", "block");
-	$(".thumb").not(".landscape").css("display", "none");
-	});
+// FILTER BUTTONS //   
+$("button").click(function(){
+    var filter = $(this).attr("id");
+    
 	
-$("#wildlife").click(function(){
-    $(".thumb").css("display", "block");
-	$(".thumb").not(".wildlife").css("display", "none");
-	});	
-	
-	
-$("#outdoor").click(function(){
-    $(".thumb").css("display", "block");
-	$(".thumb").not(".outdoor").css("display", "none");
-	});	
-	
-$("#all").click(function(){
-	$(".thumb").css("display", "block");
-	});	
-	
+	if (filter == "all") {
+	    $(".thumb").css("display", "block");
+	}
+	else {
+	    $(".thumb").css("display", "block");
+	    $(".thumb").not("." + filter).css("display", "none");
+	    $(".thumb").filter("." + filter).parent().next().addClass("curFilter");
+	}
+})
+
+
 // VIEW PHOTO LARGE //
-/*$(".imageS").click(function(){
-    $(this).next().css("display", "block");
-    $(".imageS").css("display", "none");
-    $(".filter").css("display", "none");
-	});
-*/	
 $(".imageS").click(function(){
     var imgURL = $(this).next().children().attr('src');
-     $("body").toggleClass("fullScreen");
+    var lastClicked = $(this);
+    $("body").toggleClass("fullScreen");
     $(".fullScreen").css("background-image", "url(" + imgURL + ")");
     $(".imageS").css("display", "none");
     $(".filter").css("display", "none");
-    $(".close").css("display", "block")
+    $(".close").css("display", "block");
+    $(".chevrons").css("display", "block");
+    $(this).next().addClass("curImg");
     });
 
 // CLOSE LARGE IMAGE AND GO BACK TO THUMBNAILS	
 $(".close").click(function(){
-    var imgURL = " "
     $("body").removeClass("fullScreen");
     $("body").removeAttr("style")
     $(".filter").css("display", "block");
     $(".imageS").css("display", "block");
-    $(".close").css("display", "none")
+    $(".close").css("display", "none");
+    $(".curImg").removeClass("curImg");
     });
+
+
+// GO TO NEXT IMAGE 
+$(".glyphicon-chevron-right").click(function(){
+    $(".curImg").addClass("prevImg").removeClass("curImg");
+    $(".imageL").filter(".curFilter").next().addClass("curImg");
+    var imgURL = $(".curIMG").children().attr("src");
+    $(".fullScreen").css("background-image", "url(" + imgURL + ")");
+    });
+/*
+    $(".glyphicon-chevron-right").click(function(){
+        var imgURL = $(".curImg").next().next().children().attr('src');
+        $(".curImg").addClass("prevImg").removeClass("curImg");
+        $(".prevImg").next().next().addClass("curImg");
+        $(".prevImg").removeClass("prevImg");
+        $(".fullScreen").css("background-image", "url(" + imgURL + ")");
+        });
+   
+       
+// GO TO PREVIOUS IMAGE 
+$(".glyphicon-chevron-left").click(function(){
+    var imgURL = $(".curImg").prev().prev().children().attr('src');
+        $(".curImg").addClass("prevImg").removeClass("curImg");
+        $(".prevImg").prev().prev().addClass("curImg");
+        $(".prevImg").removeClass("prevImg");
+        $(".fullScreen").css("background-image", "url(" + imgURL + ")");
+        });
+    
+*/       
+       
+       
 }); 
+
+
